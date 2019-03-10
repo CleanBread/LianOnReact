a4a5c3391338ad1368170653dcb21c363a0cdcc2
Gleb <gleb4ist@gmail.com>
2019-03-10 16:12:51 +0300

    сохранение прокрутки в компоненте Home при возвращении на него

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,18 @@ import './home.sass';
 import Slider from '../slider/Slider'
 import Filter from '../filter/Filter'
 
+let cords = ['scrollX','scrollY'];
+
 class Home extends Component {
+
+    componentWillUnmount() {
+        cords.forEach(cord => localStorage[cord] = window[cord]);
+    };
+
+    componentDidMount() {
+        window.scroll(...cords.map(cord => localStorage[cord]));
+    };
+
     render() {
         return (
             <div className="home-container">
