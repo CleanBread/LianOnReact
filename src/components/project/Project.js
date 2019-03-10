e8e88a8ad82382adef63681e64409e137ea43fdc
Gleb <gleb4ist@gmail.com>
2019-03-10 20:00:13 +0300

    минимальная версия отображения проекта готова

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -24,7 +24,22 @@ class Project extends Component {
                 <Title image={`url(${title_img})`} headline="LATEST PROJECTS" sent="We Deliver Quality"/>
                 <div className="project__inf-container">
                     <div className="inf">
-
+                        <div className="inf__head">
+                            <span className="headline">{this.state.slide.headline}</span>
+                            <span className="sub">{this.state.slide.subheading}</span>
+                        </div>
+                        <div className="inf__body">
+                            <span className="date">{this.state.slide.date}</span>
+                            <span className="text">{this.state.slide.text}</span>
+                        </div>
+                        <div className="inf__role">
+                            <span className="whitch">ROLE ON PROJECT:</span>
+                            <span className="roles">{this.state.slide.role}</span>
+                        </div>
+                        <div className="inf__role">
+                            <span className="whitch">TAGS:</span>
+                            <span className="roles">{this.state.slide.tags}</span>
+                        </div>
                     </div>
                     <div className="image" style={{backgroundImage: this.state.slide.eachSlide}}></div>
                 </div>
--- src/reducers/info.js
+++ src/reducers/info.js
@@ -19,47 +19,95 @@ const mainState = {
             date: 'February 28, 2015',
             text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
             role: 'Project Designer, Lead Designer, Markerting Head',
-            tags: 'Mobile App, UI/UX design'
+            tags: 'Mobile App, UI/UX Design'
         },
         {
             eachSlide: `url(${Item_2})`,
             status: 'mobile',
-            to: '1'
+            to: '1',
+            headline: 'Desk&Chair Creative Agency',
+            subheading: 'Branding Design',
+            date: 'May 12, 2014',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'Wordpress, Logo'
         },
         {
             eachSlide: `url(${Item_3})`,
             status: 'wordpress',
-            to: '2'
+            to: '2',
+            headline: '4Desk Agency',
+            subheading: 'Branding Design',
+            date: 'Octouber 4, 2018',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'Wordpress, Mobile App'
         },
         {
             eachSlide: `url(${Item_4})`,
             status: 'logo',
-            to: '3'
+            to: '3',
+            headline: 'Сhandelier Agency',
+            subheading: 'Branding Design',
+            date: 'July 20, 2013',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'Logo, Web Design'
         },
         {
             eachSlide: `url(${Item_5})`,
             status: 'wordpress',
-            to: '4'
+            to: '4',
+            headline: 'Corinne Creative Agency',
+            subheading: 'Branding Design',
+            date: 'December 10, 2013',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'Wordpress, UI/UX Design'
         },
         {
             eachSlide: `url(${Item_6})`,
             status: 'webDesign',
-            to: '5'
+            to: '5',
+            headline: 'Fork&Spoon Creative Agency',
+            subheading: 'Branding Design',
+            date: 'May 12, 2019',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'Web Design, Logo'
         },
         {
             eachSlide: `url(${Item_7})`,
             status: 'ux/ix',
-            to: '6'
+            to: '6',
+            headline: 'There Are Two Chairs Creative Agency',
+            subheading: 'Branding Design',
+            date: 'September 1, 2011',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'UI/UX Design, Logo'
         },
         {
             eachSlide: `url(${Item_8})`,
             status: 'branding',
-            to: '7'
+            to: '7',
+            headline: 'Сouple Creative Agency',
+            subheading: 'Branding Design',
+            date: 'November 11, 2017',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'Branding, Wordpress'
         },
         {
             eachSlide: `url(${Item_9})`,
             status: 'branding',
-            to: '8'
+            to: '8',
+            headline: 'Clock Agency',
+            subheading: 'Branding Design',
+            date: 'April 21, 2015',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.',
+            role: 'Project Designer, Lead Designer, Markerting Head',
+            tags: 'Branding, UI/UX Design'
         }
     ]
 }
