6dd612eb4051dc0adf10a45fe9dd826d95da383d
Gleb <gleb4ist@gmail.com>
2019-03-22 21:16:28 +0300

    добавлены media запросы для главной страницы

--- src/components/navbar/navbar__item/NavItem.js
+++ src/components/navbar/navbar__item/NavItem.js
@@ -6,7 +6,7 @@ class NavItem extends Component {
     render() {
         return (
             <>
-            <li>
+            <li className="nav-item">
                 <span className={this.props.span}></span>
                 <Link exact={this.props.exact} to={this.props.to} activeClassName="active">{this.props.name}</Link>
             </li>
